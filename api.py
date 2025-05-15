59f7937b90e5fcb6adb6decaffb55dc1e60f57d0
winoprg394 <winoprg68@gmail.com>
2025-05-15 12:03:29 +0200

    first commit

--- api.py
+++ api.py
@@ -92,4 +92,4 @@ def extract_info():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True,host='0.0.0.0', port=port)
